14bd62c87def3e1363227e357ed96548be7a24f2
Thomas Vigouroux <tomvig38@gmail.com>
2020-08-31 23:08:58 +0200

    fix: display full lines only and avoid duplication

--- lua/treesitter-context/internal.lua
+++ lua/treesitter-context/internal.lua
@@ -10,29 +10,40 @@ function M.update(bufnr)
   if not (preview_buf and api.nvim_win_is_valid(preview_win)) then return end
 
   local current_win = api.nvim_get_current_win()
-
   local current_node = ts_utils.get_node_at_cursor(current_win)
 
   local scopes = locals.get_scope_tree(current_node, bufnr)
 
   local text = {}
+  local displayed = {} -- Already displayed lines
+  local toprow = vim.fn.line("w0") - 1
+  local max_width = -math.huge
 
   for _, scope in ipairs(scopes) do
-    if scope:parent() then
-      local scope_first_line = ts_utils.get_node_text(scope, bufnr)[1]
-      table.insert(text, 1, scope_first_line)
+    local start_row, _, _ = scope:start()
+
+    -- FIXME(vigoux): this is not actually what we mean, some weird things can happen
+    -- For example :
+    -- if (test // Only this will be shown
+    --  || test) {
+    -- }
+
+    local line = api.nvim_buf_get_lines(bufnr, start_row, start_row+1, false)[1]
+
+    if scope:parent() and not displayed[start_row] and toprow > start_row then
+      table.insert(text, 1, line)
+      displayed[start_row] = 1
+
+      max_width = math.max(max_width, #line)
     end
   end
 
   api.nvim_buf_set_lines(preview_buf, 0, -1, false, text)
-  local win_height
-  if #text == 0 then
-    win_height = 1
-  else
-    win_height = #text
-  end
 
-  api.nvim_win_set_config(preview_win, {height = win_height})
+  api.nvim_win_set_config(preview_win, {
+    height = math.max(1, #text),
+    width = math.max(max_width, 60)
+  })
 
   return text
 end
@@ -47,7 +58,7 @@ function M.setup(bufnr)
     width = 60,
     height = 1,
     row = 0,
-    col = columns,
+    col = 9000, -- To always have it top right
     focusable = false,
     style = "minimal"
   })
